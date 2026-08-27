7d835db2fd70558dc3fb11d022585ba2dfe094ee
pedrostuart <pedrohsmoura897@gmail.com>
2026-08-26 22:19:39 -0300

    .

--- src/localizacao/localizacao.service.ts
+++ src/localizacao/localizacao.service.ts
@@ -2,6 +2,7 @@ import { Injectable, BadRequestException/*quando a requisição da errado(404)*/
 import { HttpService } from '@nestjs/axios';/*Nossa REquisição pra api(trabalha com Observable)*/
 
 import { lastValueFrom } from 'rxjs';//na api do cep tem varias informações(varios valores em diferentes lugares), o last transforma o observable em uma Promisse que podemos utilizar atraves do await
+import { count } from 'console';
 
 @Injectable()
 export class LocalizacaoService {
@@ -47,6 +48,61 @@ export class LocalizacaoService {
                 throw erro
             }
             throw new ServiceUnavailableException('Não foi possivel consultar o serviço de CEP')
+        }
+    }
+    //Função resposável por consultar a localização por cidade
+    async buscarCidade(cidade:string){
+        //Evita buscas vazias enviadas para API
+        if(!cidade || cidade.trim().length < 2){
+            throw new BadRequestException('Informe uma cidade válida')
+        }
+        try{
+            //EncodeUri prepara o texto para ser enviado para a API(sendo utilizado denrto de uma url). Dessa forma, conseguimos passar "São Paulo" normalmente atraves da URL
+            
+            const cidadeCodificada = encodeURIComponent(cidade.trim())
+
+            //Enviamos a requisição para a API de geolocalização onde
+            //os parametros serão:
+            // nome da cidade
+            //count 1: somente o primeiro resultado que for retornado
+            //language: informações traduzidas para o portugues
+            //contryCode: fazemos busca dentro do brasil
+
+            const resposta = await lastValueFrom (
+                this.httpService.get('https://geocoding-api.open-meteo.com/v1/serch',{
+                    params: {
+                        nome: cidade.trim(),
+                        count: 1,
+                        language: 'pt',
+                        contryCode: 'BR'
+                    }
+                })
+            )
+            //recebe os dados da resposta
+            const dados = resposta.data
+
+            //A API vai retornar nos dados dentro de um array chamado results: []. Se este não existir ou estiver vazio, informamos ao usuario
+            if(!dados.results || dados.results.length === 0){
+                throw new NotFoundException('Localidade não encontrada')
+            }
+
+            //Pegamos o primeiro resultado trazido
+            const localizacao = dados.results[0]
+            //Reotrnamos de forma visual os dados para que o usuario veja
+            return{
+                cidade: localizacao.name,
+                estado: localizacao.admin1,//representa estado/regiao dentro da api
+                pais: localizacao.contry,
+                latitude: localizacao.latitude,
+                longitude: localizacao.longitude
+            }
+        } catch(erro){
+            if(
+                erro instanceof NotFoundException || erro instanceof BadRequestException
+            ){
+                throw erro
+            }
+            throw new ServiceUnavailableException("Não foi possivel consultar o serviço de localização")
         }
     }
 }
